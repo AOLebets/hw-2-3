584bdc606554190bcd153ec3ac437a3b8bd09a53
AOLebets <89778658+AOLebets@users.noreply.github.com>
2022-11-13 17:53:28 +0200

    Add files via upload

---
+++ Lebets_HW_Module2_3/find_max_projects_client.sql
@@ -0,0 +1,16 @@
+SELECT MAX(PROJECT_COUNT) FROM
+( SELECT c.ID, c.NAME, COUNT( p.ID ) AS PROJECT_COUNT FROM CLIENT c JOIN
+PROJECT p ON c.ID = p.CLIENT_ID  GROUP BY c.ID, c.NAME ORDER BY 3 DESC ) F;
+
+SELECT c.ID
+      ,c.NAME
+      ,COUNT( p.ID ) AS PROJECT_COUNT
+FROM   CLIENT c
+JOIN   PROJECT p
+    ON c.ID = p.CLIENT_ID
+GROUP BY c.ID
+        ,c.NAME
+HAVING PROJECT_COUNT = ( SELECT MAX( PROJECT_COUNT )
+                         FROM   ( SELECT COUNT( p.ID ) AS PROJECT_COUNT
+                                  FROM PROJECT p
+                                  GROUP BY p.CLIENT_ID ) Fs );
